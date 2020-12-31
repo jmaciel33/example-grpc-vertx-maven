cd2623d24d5c65dc51bab504c65a808633e1b759
Maciel, Julio Cesar <jmaciel33@gmail.com>
2020-12-31 14:00:26 -0300

    firs commit

---
+++ src/main/java/com/example/Server.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import com.example.util.Runner;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.examples.helloworld.VertxGreeterGrpc;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+import io.vertx.grpc.VertxServer;
+import io.vertx.grpc.VertxServerBuilder;
+
+public class Server extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    Runner.runExample(Server.class);
+  }
+
+  @Override
+  public void start() {
+    VertxServer server = VertxServerBuilder.forAddress(vertx, "localhost", 8080).addService(new VertxGreeterGrpc.GreeterVertxImplBase() {
+      @Override
+      public Future<HelloReply> sayHello(HelloRequest request) {
+        System.out.println("Hello " + request.getName());
+        return Future.succeededFuture(HelloReply.newBuilder().setMessage(request.getName()).build());
+      }
+    }).build();
+    server.start(ar -> {
+      if (ar.succeeded()) {
+        System.out.println("gRPC service started");
+      } else {
+        System.out.println("Could not start server " + ar.cause().getMessage());
+      }
+    });
+  }
+}
